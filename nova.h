33fcbe3ebd55ec6774c44240c936b3817656a70a
strahochen <strahochen@foxmail.com>
2025-04-22 19:44:29 +0800

    test(stats): only test write time

--- nova.h
+++ nova.h
@@ -1028,15 +1028,15 @@ static inline size_t do_nova_nvmm_read(struct super_block *sb, void *ubuf_dest,
 {
 	size_t left;
 	INIT_TIMING(memcpy_time);
-	INIT_TIMING(bd_memcpy_time);
+	// INIT_TIMING(bd_memcpy_time);
 	INIT_TIMING(delegation_time);
 
 	if (zero) {
 		nova_dbg_delegation("do __clear_user to fill zero\n");
 		NOVA_START_TIMING(memcpy_r_nvmm_t, memcpy_time);
-		NOVA_START_META_TIMING(bd_memcpy_r_t, bd_memcpy_time);
+		// NOVA_START_META_TIMING(bd_memcpy_r_t, bd_memcpy_time);
 		left = __clear_user(ubuf_dest, bytes);
-		NOVA_END_META_TIMING(bd_memcpy_r_t, bd_memcpy_time);
+		// NOVA_END_META_TIMING(bd_memcpy_r_t, bd_memcpy_time);
 		NOVA_END_TIMING(memcpy_r_nvmm_t, memcpy_time);
 		return left;
 	}
@@ -1047,9 +1047,9 @@ static inline size_t do_nova_nvmm_read(struct super_block *sb, void *ubuf_dest,
 				    (unsigned long)kmem_src,
 				    (unsigned long)ubuf_dest);
 		NOVA_START_TIMING(memcpy_r_nvmm_t, memcpy_time);
-		NOVA_START_META_TIMING(bd_memcpy_r_t, bd_memcpy_time);
+		// NOVA_START_META_TIMING(bd_memcpy_r_t, bd_memcpy_time);
 		left = __copy_to_user(ubuf_dest, kmem_src, bytes);
-		NOVA_END_META_TIMING(bd_memcpy_r_t, bd_memcpy_time);
+		// NOVA_END_META_TIMING(bd_memcpy_r_t, bd_memcpy_time);
 		NOVA_END_TIMING(memcpy_r_nvmm_t, memcpy_time);
 	} else {
 		nova_dbg_delegation("do delegation\n");
