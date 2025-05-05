b79ad50320536426ef13d7a4f75f41a24faddce0
strahochen <strahochen@foxmail.com>
2025-05-05 15:36:10 +0800

    to(stats): data time

--- stats.c
+++ stats.c
@@ -287,7 +287,7 @@ static void nova_print_meta_stats(struct super_block *sb)
 	nova_info("write_total: %llu\n", Timingmetastats[bd_cow_write_t]);
 	nova_info("write_meta: %llu\n", Timingmetastats[bd_cow_write_t] -
 						Timingmetastats[bd_wait_data_t]);
-	nova_info("write_data: %llu\n", Timingmetastats[bd_cow_write_t]);
+	nova_info("write_data: %llu\n", Timingmetastats[bd_wait_data_t]);
 }
 
 void nova_get_timing_stats(void)
