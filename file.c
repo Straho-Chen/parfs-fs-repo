e15f3a377400630ed063926b1625b81499e3fd59
strahochen <strahochen@foxmail.com>
2025-05-12 16:23:57 +0800

    to(dcsum): add dcsum

--- file.c
+++ file.c
@@ -926,21 +926,21 @@ static ssize_t do_nova_cow_file_write(struct file *filp, const char __user *buf,
 				 head, tail);
 
 		// we do data csum on cow write, no need is_dele check
-		// 		if (is_dele && (data_csum > 0 || data_parity > 0)) {
-		// 			/* calculate data checksum and write csum to pmem */
-		// 			NOVA_START_META_TIMING(bd_data_csum_t,
-		// 					       bd_data_csum_time);
-		// #if NOVA_KERNEL_COPY_USER_BUFFER
-		// 			ret = nova_protect_file_data(sb, inode, pos, bytes,
-		// 						     ubuf_copy, blocknr);
-		// #else
-		// 			ret = nova_protect_file_data(sb, inode, pos, bytes,
-		// 						     (char *)buf, blocknr);
-		// #endif
-		// 			NOVA_END_META_TIMING(bd_data_csum_t, bd_data_csum_time);
-		// 			if (ret)
-		// 				goto out;
-		// 		}
+		if (is_dele && (data_csum > 0 || data_parity > 0)) {
+			/* calculate data checksum and write csum to pmem */
+			NOVA_START_META_TIMING(bd_data_csum_t,
+					       bd_data_csum_time);
+#if NOVA_KERNEL_COPY_USER_BUFFER
+			ret = nova_protect_file_data(sb, inode, pos, bytes,
+						     ubuf_copy, blocknr);
+#else
+			ret = nova_protect_file_data(sb, inode, pos, bytes,
+						     (char *)buf, blocknr);
+#endif
+			NOVA_END_META_TIMING(bd_data_csum_t, bd_data_csum_time);
+			if (ret)
+				goto out;
+		}
 
 		copied = bytes;
 		if (pos + copied > inode->i_size)
