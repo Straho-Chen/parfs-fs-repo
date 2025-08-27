7a40732bb33e1282865f5a8be1c76a88cde8dd50
strahochen <strahochen@foxmail.com>
2025-08-27 14:24:43 +0800

    to(crc): not use asm perf

--- nova.h
+++ nova.h
@@ -82,43 +82,14 @@ static inline __le32 nova_mask_flags(umode_t mode, __le32 flags)
 
 static inline u32 nova_crc32c(u32 crc, const u8 *data, size_t len)
 {
-	u8 *ptr = (u8 *)data;
-	u64 acc = crc; /* accumulator, crc32c value in lower 32b */
 	u32 csum;
 
-	/* x86 instruction crc32 is part of SSE-4.2 */
-	if (static_cpu_has(X86_FEATURE_XMM4_2)) {
-		/* This inline assembly implementation should be equivalent
-		 * to the kernel's crc32c_intel_le_hw() function used by
-		 * crc32c(), but this performs better on test machines.
-		 */
-		while (len > 8) {
-			asm volatile(/* 64b quad words */
-				     "crc32q (%1), %0"
-				     : "=r"(acc)
-				     : "r"(ptr), "0"(acc));
-			ptr += 8;
-			len -= 8;
-		}
-
-		while (len > 0) {
-			asm volatile(/* trailing bytes */
-				     "crc32b (%1), %0"
-				     : "=r"(acc)
-				     : "r"(ptr), "0"(acc));
-			ptr++;
-			len--;
-		}
-
-		csum = (u32)acc;
-	} else {
-		/* The kernel's crc32c() function should also detect and use the
+	/* The kernel's crc32c() function should also detect and use the
 		 * crc32 instruction of SSE-4.2. But calling in to this function
 		 * is about 3x to 5x slower than the inline assembly version on
 		 * some test machines.
 		 */
-		csum = crc32c(crc, data, len);
-	}
+	csum = crc32c(crc, data, len);
 
 	return csum;
 }
